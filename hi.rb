96910c4a1d797d91a145042e6c3e21f4fbecf975
Nisha Sehn <nishasehn@gmail.com>
2012-12-05 11:37:40 -0800

    Redone WhoIsServer with checkboxes.

--- hi.rb
+++ hi.rb
@@ -12,15 +12,18 @@ end
 
 post '/whois' do #the sinatra will respond to this post /whois
 	puts params #not needed. This tells us, the developer, what is in params
-	@state = "unavailable" #instance variable that is set to unavailabe as a default value
-	@domain = params[:domain] #params contains all the data that came from the post request which in this case came from the index.erb code
+	# @state = "unavailable" #instance variable that is set to unavailabe as a default value
+	domain = params[:domain] #params contains all the data that came from the post request which in this case came from the index.erb code
 				# the instance variable does not have to be the same name is the :word. :domain could be any word as long as it is exists in params (it exists in the index.erb code)
-  				
+  	domaintype = params[:domaintype]
   	#perform whois lookup
-  	query = Whois::query(@domain) #Query is a part of the whois gem
-  
-  	@state = "available" if query.available?
-  
-  	erb :result
+  	domain_extensions = params[:domaintype].values
 
+  	@query = []
+  	domain_extensions.each do |extension|
+  		result = Whois::query(domain + extension) #Query is a part of the whois gem
+  		@query << result
+  	end
+
+  	erb :result
 end
